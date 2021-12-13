613dcef3c2dc636f9e990aa8e887c537e4f359a0
Azryel <AzryelShroud@gmail.com>
2021-12-13 02:36:06 +0100

    allow iframe fullscreen

--- src/components/VideoFrame.tsx
+++ src/components/VideoFrame.tsx
@@ -7,8 +7,8 @@ const VideoFrame = ({ videos }: { videos: videos }) => {
         <div className="VideoFrame" >
             {
                 ( videos.results[0].site === "YouTube" ) ?
-                <iframe width="100%" sandbox="allow-scripts allow-same-origin allow-presentation" className="video" src={ `https://www.youtube.com/embed/${ videos.results[0].key }` } title={ videos.results[0].name } ></iframe>
-                : <iframe width="100%" className="video" src={ `https://vimeo.com/${ videos.results[0].id }` } title={ videos.results[0].name } ></iframe>
+                <iframe width="100%" sandbox="allow-scripts allow-same-origin allow-presentation" className="video" src={ `https://www.youtube.com/embed/${ videos.results[0].key }` } title={ videos.results[0].name } allow="fullscreen;" ></iframe>
+                : <iframe width="100%" className="video" src={ `https://vimeo.com/${ videos.results[0].id }` } title={ videos.results[0].name } allow="fullscreen;" ></iframe>
             }
         </div>
     )
